09cb126f2aa78c30b73687c4ed008d35bb499ebe
kalpesh <kbarde@paypal.com>
2018-01-02 23:10:38 -0800

    Deleting a Node

--- Practice/src/com/testing1/LinkedList.java
+++ Practice/src/com/testing1/LinkedList.java
@@ -12,16 +12,45 @@ public class LinkedList {
         }
     }
 
+    public void push(int data) {
+        final LinkedNode startNode = new LinkedNode(data);
+        startNode.next = this.head;
+        this.head = startNode;
+    }
+
+    public void delete(int key) {
+
+        LinkedNode temp = this.head, prev = null;
+
+        if (temp != null && temp.data == key) {
+            this.head = temp.next;
+            return;
+        }
+
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            return;
+        }
+        prev.next = temp.next;
+    }
+
     public static void main(String[] args) {
 
         final LinkedList llist2 = new LinkedList();
         llist2.head = new LinkedNode(4);
         final LinkedNode second = new LinkedNode(5);
         final LinkedNode third = new LinkedNode(6);
-
         llist2.head.next = second;
         second.next = third;
-
+        llist2.push(3);
+        llist2.printList();
+        // consider I want to delete 5
+        llist2.delete(5);
+        System.out.println(" ================================================ ");
         llist2.printList();
 
     }
